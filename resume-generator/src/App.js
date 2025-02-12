17b5df47ca552ae5e84309c21eac6d945f356f24
Prince Tiwari <princetiwari.profes@gmail.com>
2025-02-12 22:10:37 +0530

    frontend

---
+++ resume-generator/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600">Resume Generator</h1>
+    </div>
+  );
+}
+
+export default App;
