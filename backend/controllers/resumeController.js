d7c7f52d2f84c4235ad4f76acbfe669c04b4d453
Prince Tiwari <princetiwari.profes@gmail.com>
2025-02-16 22:06:28 +0530

    backend

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -15,6 +15,28 @@ const getAllData = async (req, res) => {
     }
 };
 
+// Controller for Basic Information
+const addOrUpdateBasicInfo = async (req, res) => {
+    try {
+        const { uniqueId, basicInfo } = req.body;
+        if (!uniqueId) {
+            return res.status(400).json({ message: "uniqueId is required" });
+        }
+        const updatedResume = await Resume.findOneAndUpdate(
+            { uniqueId },
+            {
+                $set: { basicInfo },
+            },
+            { upsert: true, new: true }
+        );
+        res.status(200).json({ message: "Resume saved successfully", resume: updatedResume });
+    } catch (error) {
+        res.status(500).json({ message: "Error saving resume", error });
+    }
+};
+
 module.exports = {
-    getAllData
+    getAllData,
+    //Basic Info
+    addOrUpdateBasicInfo
 }
--- backend/routes/resume.route.js
+++ backend/routes/resume.route.js
@@ -1,7 +1,10 @@
-const { getAllData } = require('../controllers/resumeController');
+const { getAllData, addOrUpdateBasicInfo } = require('../controllers/resumeController');
 
 const router = require('express').Router();
 
 router.get('/:uniqueId', getAllData);
+
+// Basic Info
+router.post('/basic-info/add-or-update', addOrUpdateBasicInfo);
 
 module.exports = router;
