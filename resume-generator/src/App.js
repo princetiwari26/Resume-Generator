6d7a59ecfca88ebbe82475a62819a1e130d2bfd3
Prince Tiwari <princetiwari.profes@gmail.com>
2025-02-18 23:21:35 +0530

    frontend

--- resume-generator/src/App.js
+++ resume-generator/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [uniqueId, setUniqueId] = useState(null);
+
+  useEffect(() => {
+    const storedId = localStorage.getItem("uniqueId");
+    if (storedId) {
+      setUniqueId(storedId);
+    } else {
+      axios.get("http://localhost:5000/generate-id").then((res) => {
+        setUniqueId(res.data.uniqueId);
+        localStorage.setItem("uniqueId", res.data.uniqueId);
+      });
+    }
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">Resume Generator</h1>
+    <div className='bg-gradient-to-r from-blue-400 to-purple-500 p-4 grid justify-center'>
+      <h1 className='text-4xl font-bold text-white text-center mt-4'>Resume Generator</h1>
+      <div className='grid md:flex items-center justify-center mt-5 md:space-x-24'>
+        <div className='text-white text-lg font-bold bg-slate-800 rounded-lg p-2 border-2'>ID - <span>{uniqueId}</span></div>
+      </div>
     </div>
   );
 }
